fb4a19fb0d0a8cf91d5bd7ecdca63087066a6c48
Hyerin Kim <kdjsanh0217@naver.com>
2024-05-20 11:30:46 +0900

    finished

--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -1,18 +1,21 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
-/**
- * 2초 지연을 시키는 함수입니다 (비동기 작업).
- */
 import { waitTwoSeconds } from '../../utils';
 
+// 2초 지연을 시키는 thunk 함수 정의
 export const __addToDo = createAsyncThunk(
   '__addToDo',
-  async (payload, thunkAPI) => {}
+  async (payload) => {
+    await waitTwoSeconds(); // 2초 지연
+    return payload; // payload 반환
+  }
 );
 
 export const __deleteTodo = createAsyncThunk(
   '__deleteToDo',
-  async (payload, thunkAPI) => {}
+  async (payload) => {
+    await waitTwoSeconds(); // 2초 지연
+    return payload; // payload 반환
+  }
 );
 
 const initialState = {
@@ -23,8 +26,22 @@ const todosSlice = createSlice({
   name: 'todos',
   initialState,
   reducers: {
-    addTodo: (state, action) => {},
-    deleteTodo: (state, action) => {},
+    // 일반적인 동기적인 액션은 여기에 추가
+    addTodo: (state, action) => {
+      state.list.push(action.payload); // 투두 리스트에 항목 추가
+    },
+    deleteTodo: (state, action) => {
+      state.list = state.list.filter(todo => todo.id !== action.payload); // 투두 리스트에서 항목 삭제
+    },
+  },
+  extraReducers: builder => {
+    // 비동기 액션 처리
+    builder.addCase(__addToDo.fulfilled, (state, action) => {
+      state.list.push(action.payload); // 투두 리스트에 항목 추가
+    });
+    builder.addCase(__deleteTodo.fulfilled, (state, action) => {
+      state.list = state.list.filter(todo => todo.id !== action.payload); // 투두 리스트에서 항목 삭제
+    });
   },
 });
 
